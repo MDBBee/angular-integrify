92dacbb623c9449654637cd419c484de3c86fde4
MDBBee <bobbyugbebor@gmail.com>
2026-01-18 07:53:17 +0200

    Dashboard page2

--- src/app/pages/dash/dash.ts
+++ src/app/pages/dash/dash.ts
@@ -1,17 +1,17 @@
 import { Component, computed, inject, OnInit } from '@angular/core';
 import { Store } from '../../services/store';
 import { Product } from '../../models/product.type';
-import { TitleCasePipe } from '@angular/common';
+import { TitleCasePipe, NgClass } from '@angular/common';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideStar } from '@ng-icons/lucide';
+import { lucideStar, lucideStarHalf } from '@ng-icons/lucide';
 import { HlmIcon } from '@spartan-ng/helm/icon';
 
 @Component({
   selector: 'app-dash',
-  imports: [TitleCasePipe, HlmIcon, NgIcon],
+  imports: [TitleCasePipe, HlmIcon, NgIcon, NgClass],
   templateUrl: './dash.html',
   styleUrl: './dash.css',
-  providers: [provideIcons({ lucideStar })],
+  providers: [provideIcons({ lucideStar, lucideStarHalf })],
 })
 export class Dash implements OnInit {
   storeService = inject(Store);
@@ -21,5 +21,20 @@ export class Dash implements OnInit {
     this.storeService.findAllProducts().subscribe((products) => {
       this.storeService.products.set(products);
     });
+  }
+
+  getStars(rate: number) {
+    const stars = [];
+    const fullStars = Math.floor(rate);
+    const fractional = rate - fullStars;
+
+    const hasHalfStar = fractional >= 0.1;
+
+    for (let i = 0; i < fullStars; i++) {
+      stars.push('lucideStar');
+    }
+    if (hasHalfStar) stars.push('lucideStarHalf');
+
+    return stars;
   }
 }
